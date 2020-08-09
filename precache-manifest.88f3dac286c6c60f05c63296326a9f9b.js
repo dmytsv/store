31361e969822ba331d893295332c134a32110d36
Dmitry Tsuprunov <dtsuprunov@pixsystem.com>
2020-08-08 21:54:12 -0400

    Updates

---
+++ precache-manifest.88f3dac286c6c60f05c63296326a9f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342ec5c9f0909d01b156f06f90cc95de",
+    "url": "/ajstore/index.html"
+  },
+  {
+    "revision": "00699b393450f58c290e",
+    "url": "/ajstore/static/css/main.f2a66b32.chunk.css"
+  },
+  {
+    "revision": "df40c650c0b54c4eab06",
+    "url": "/ajstore/static/js/2.a2163ac0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ajstore/static/js/2.a2163ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00699b393450f58c290e",
+    "url": "/ajstore/static/js/main.7f560845.chunk.js"
+  },
+  {
+    "revision": "983304292e5b4d7f1f7b",
+    "url": "/ajstore/static/js/runtime-main.db217b20.js"
+  }
+]);
